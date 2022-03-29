a869cb6d57d8969e223c90911d971d58fb7deb6a
nando-carvalho <fernando@elosistemas.inf.br>
2022-03-29 16:54:14 -0300

    Conexao ao banco e envio de um insert com sucesso!

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+    titulo: 'Coleção de sucesso',
+    descricao: 'Persista, que você irá conseguir!.'
+})
+    .then(dados =>  console.log(dados))
+    .catch(e => console.log(e));
+
 exports.paginaInicial = (req, res) => {    
         res.render('index');
 };

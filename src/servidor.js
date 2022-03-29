6757387619591b653815c03f7cc9570d7dc83733
nando-carvalho <fernando@elosistemas.inf.br>
2022-03-29 17:15:17 -0300

    limpeza servidor.js

--- src/servidor.js
+++ src/servidor.js
@@ -8,17 +8,13 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTIONSTRING,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false
+    useUnifiedTopology: true,    
   })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
 
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
-// const flash = require('connect-flash');
 // const bodyParser = require('body-parser');
 const routes = require('./routes');
 const path = require('path');
@@ -29,18 +25,6 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.static(path.resolve(__dirname, 'public')));
-// const sessionOptions = session({
-//   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
-//   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//     maxAge: 1000 * 60 * 60 * 24 * 7,
-//     httpOnly: true
-//   }
-// });
-// app.use(sessionOptions);
-// app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
